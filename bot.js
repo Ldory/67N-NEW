fcf3af84a32fef5b501d7e70afeaa3da84cb1a07
Ldory <40350674+Ldory@users.noreply.github.com>
2019-06-22 19:42:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,5 +5,28 @@ const Discord = require("discord.js");
           console.log('Im Ready!');
   
         });
+var prefix = "!";
+client.on("message", (message) => {
+  const [command, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+  if (command === "bc") {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("ليس لديك صلاحية ادمن");
+    let content = message.content.slice((prefix.length + command.length));
+    if (!content) return message.channel.send("الرسالة فارغة");
+    message.channel.send("sending").catch(() => {}).then((response)=> {
+      var users = [];
+      var res = 0, ref = 0;
+      message.guild.members.map(user => users.push(user))
+      let size = users.length;
+      users.map((user, idx) => {
+        setTimeout(() => {
+          user.send(content).catch(()=>ref++).then(() => {
+            res++
+          });
+          response.edit((ref + res) == size ? `Done !!!!!, ${res} done and ${ref} undone from ${size}` : `${res} done and ${ref} undone from ${size}`).catch(e=>e)
+        }, 7000 / size * idx);
+      })
+    })
+  }
+});
 
 kboosh.login(process.env.BOT_TOKEN)
