68daff92acf15dba43058d08f742eed5df80cb69
Ldory <40350674+Ldory@users.noreply.github.com>
2019-06-23 08:12:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,31 +4,5 @@ const client = new Discord.Client();
 const prefix = "-"
 
 
-client.on('message', message => {
-   let embed = new Discord.RichEmbed()
-
-    let args = message.content.split(' ').slice(1).join(' ');
-     if(!message.channel.guild) return;
-if(message.content.split(' ')[0] == '-bc') { //الامر
-         message.react("✔️")
-          let embed = new Discord.RichEmbed()
-    .setColor("#FF00FF")
-    .setThumbnail(message.author.avatarURL)   
-                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
-                 message.channel.sendEmbed(embed);
-        message.guild.members.forEach(m => {
-            var bc = new Discord.RichEmbed()
-.addField('**● المرسل  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
-            .addField('***● السيرفر  :***', `*** → ${message.guild.name}***`)               
-    .setColor('#ff0000')
-                 .addField('ّ', args)
-            m.send(``,{embed: bc});
-        });
-    }
-})
-client.on("guildMemberAdd", member => {
-  client.channels.find('id', '592019960469913605').send(`**welcome to 67N Server : [ ${member} ]**`)
-});
-
 
 kboosh.login(process.env.BOT_TOKEN)
