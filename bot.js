1c5258923ec09a78b641d46855da3dc9c0876ae3
Ldory <40350674+Ldory@users.noreply.github.com>
2019-06-23 11:44:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`67N SYSTEM.`,'https://www.twitch.tv/v5bz');
-  console.log('---------------');
-  console.log(' Bot Is Online')
-  console.log('---------------')
 });
 const prefix = "-"
 client.on('message',message =>   {
