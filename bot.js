b8527123f7d46d48ec0a62189d2622113da5d653
Ldory <40350674+Ldory@users.noreply.github.com>
2019-06-23 07:46:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,28 +4,28 @@ const client = new Discord.Client();
 const prefix = "-"
 
 
-client.on("message", (message) => {
-  const [command, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
-  if (command === "bc") {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("ليس لديك صلاحية ادمن");
-    let content = message.content.slice((prefix.length + command.length));
-    if (!content) return message.channel.send("الرسالة فارغة");
-    message.channel.send("sending").catch(() => {}).then((response)=> {
-      var users = [];
-      var res = 0, ref = 0;
-      message.guild.members.map(user => users.push(user))
-      let size = users.length;
-      users.map((user, idx) => {
-        setTimeout(() => {
-          user.send(content).catch(()=>ref++).then(() => {
-            res++
-          });
-          response.edit((ref + res) == size ? `Done !!!!!, ${res} done and ${ref} undone from ${size}` : `${res} done and ${ref} undone from ${size}`).catch(e=>e)
-        }, 7000 / size * idx);
-      })
-    })
-  }
-});
+client.on('message', message => {
+   let embed = new Discord.RichEmbed()
+
+    let args = message.content.split(' ').slice(1).join(' ');
+     if(!message.channel.guild) return;
+if(message.content.split(' ')[0] == '-bc') { //الامر
+         message.react("✔️")
+          let embed = new Discord.RichEmbed()
+    .setColor("#FF00FF")
+    .setThumbnail(message.author.avatarURL)   
+                                      .addField('تم الارسال بواسطة :', "<@" + message.author.id + ">")
+                 message.channel.sendEmbed(embed);
+        message.guild.members.forEach(m => {
+            var bc = new Discord.RichEmbed()
+.addField('**● المرسل  :**', `*** → ${message.author.username}#${message.author.discriminator}***`)
+            .addField('***● السيرفر  :***', `*** → ${message.guild.name}***`)               
+    .setColor('#ff0000')
+                 .addField('ّ', args)
+            m.send(``,{embed: bc});
+        });
+    }
+})
 client.on("guildMemberAdd", member => {
   client.channels.find('id', '592019960469913605').send(`**welcome to 67N Server : [ ${member} ]**`)
 });
